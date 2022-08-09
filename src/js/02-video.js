c1d920051b887f9f4635110b96677111832dcfb7
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-09 15:29:31 +0300

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.setCurrentTime(localStorage.getItem("currentPlayerTime"));
+
+player.on('timeupdate', throttle((timeData) => localStorage.setItem("currentPlayerTime", timeData.seconds), 1000));
