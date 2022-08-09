41384cc044e7104cb8f29135d2714de9e8f4f036
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-09 16:31:02 +0300

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,13 +4,9 @@ const form = document.querySelector('form');
 const submit = document.querySelector('button');
 let feedback = JSON.parse(localStorage.getItem("feedback-form-state"));
 
-if (feedback.email) {
-    form[0].value = feedback.email;
-};
+if (feedback.email) {form[0].value = feedback.email;};
 
-if (feedback.message) {
-    form[1].value = feedback.message;
-};
+if (feedback.message) {form[1].value = feedback.message;};
 
 form.addEventListener('input', throttle(() => {
     feedback.email = form[0].value;
@@ -25,4 +21,4 @@ submit.addEventListener('click', (event) => {
     form[0].value = '';
     form[1].value = '';
     localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
-})
+});
