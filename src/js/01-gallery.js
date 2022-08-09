1d69f9fe742153bd34ee2f87ba6d2f84d42b37af
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-09 12:59:58 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const trgForAdd = document.querySelector('.gallery');
+
+galleryGen(galleryItems);
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+function galleryGen(galleryItems) {
+    const dataSet = galleryItems.reduce((acc, { preview, original, description }) => {
+        return acc +=
+            `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+    }, '');
+    trgForAdd.innerHTML = dataSet;
+};
