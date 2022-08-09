59be273cb99711016097f577d9779a8f86ddb017
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-09 16:28:29 +0300

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,6 +4,7 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-player.setCurrentTime(localStorage.getItem("currentPlayerTime"));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
-player.on('timeupdate', throttle((timeData) => localStorage.setItem("currentPlayerTime", timeData.seconds), 1000));
+player.on('timeupdate', throttle((timeData) =>
+    localStorage.setItem("videoplayer-current-time", timeData.seconds), 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('form');
+const submit = document.querySelector('button');
+let feedback = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+if (feedback.email) {
+    form[0].value = feedback.email;
+};
+
+if (feedback.message) {
+    form[1].value = feedback.message;
+};
+
+form.addEventListener('input', throttle(() => {
+    feedback.email = form[0].value;
+    feedback.message = form[1].value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+}, 500));
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(feedback);
+    feedback = {};
+    form[0].value = '';
+    form[1].value = '';
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+})
